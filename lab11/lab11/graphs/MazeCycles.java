1e72458d787baec06d01f91b0edee8e60747f73a
tinge <2788655279@qq.com>
2021-05-13 19:59:37 +0800

    lab11 completed

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,7 +1,9 @@
 package lab11.graphs;
 
+import java.util.Random;
+
 /**
- *  @author Josh Hug
+ *  @author tinge
  */
 public class MazeCycles extends MazeExplorer {
     /* Inherits public fields:
@@ -10,15 +12,39 @@ public class MazeCycles extends MazeExplorer {
     public boolean[] marked;
     */
 
+    private int[] parent;
+
     public MazeCycles(Maze m) {
         super(m);
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        parent = new int[maze.V()];
+        Random rand = new Random();
+        int startX = rand.nextInt(maze.N());
+        int startY = rand.nextInt(maze.N());
+        int s = maze.xyTo1D(startX, startY);
+        marked[s] = true;
+        distTo[s] = 0;
+        parent[s] = s;
+        dfs(s);
     }
 
-    // Helper methods go here
+    private void dfs(int v) {
+        for (int u : maze.adj(v)) {
+            if (!marked[u]) {
+                marked[u] = true;
+                distTo[u] = distTo[v] + 1;
+                announce();
+                parent[u] = v;
+                dfs(u);
+            } else if (u != parent[v]) {
+                return;
+            }
+        }
+    }
+
+
 }
 
